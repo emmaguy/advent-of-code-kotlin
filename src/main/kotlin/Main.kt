a52b18529c48da3de54097be8d4103f447a1921b
Emma Vanbrabant <emmag87@gmail.com>
2021-12-02 20:05:43 +0000

    Intellij & day2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import day1.day1
+import day2.day2
+
+fun main(args: Array<String>) {
+    day1()
+    day2()
+}
---
+++ src/main/kotlin/day2/day2.kt
@@ -0,0 +1,55 @@
+package day2
+
+import java.io.File
+
+internal val submarineSampleInput = listOf(
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2"
+)
+internal val submarineInput = File("src/main/resources/day2.txt").readLines()
+
+fun day2() {
+    part2(submarineInput)
+}
+
+internal fun part2(input: List<String>) {
+    var horizontalPosition = 0
+    var depth = 0
+    var aim = 0
+
+    for (instruction in input) {
+        val (direction, distanceStr) = instruction.split(' ')
+        val distance = distanceStr.toInt()
+        when (direction) {
+            "forward" -> {
+                horizontalPosition += distance
+                depth += aim * distance
+            }
+            "down" -> aim += distance
+            "up" -> aim -= distance
+        }
+    }
+
+    println("horizontal position * depth: ${horizontalPosition * depth}")
+}
+
+internal fun part1(input: List<String>) {
+    var horizontalPosition = 0
+    var depth = 0
+
+    for (instruction in input) {
+        val (direction, distanceStr) = instruction.split(' ')
+        val distance = distanceStr.toInt()
+        when (direction) {
+            "forward" -> horizontalPosition += distance
+            "down" -> depth += distance
+            "up" -> depth -= distance
+        }
+    }
+
+    println("horizontal position * depth: ${horizontalPosition * depth}")
+}
