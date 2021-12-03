c5bb027932b54bd73c2f6f0e4f74bb7c95406a0d
Emma Vanbrabant <emmag87@gmail.com>
2021-12-03 11:06:28 +0000

    part 2

--- src/main/kotlin/2021/day3/day3.kt
+++ src/main/kotlin/2021/day3/day3.kt
@@ -7,20 +7,65 @@ internal val sampleInput =
 internal val diagnosticReport = File("src/main/resources/2021/day3.txt").readLines()
 
 fun day3() {
-    part1(diagnosticReport)
+    part2(diagnosticReport)
 }
 
-internal fun part2() {
+internal fun part2(diagnosticReport: List<String>) {
+    val oxygenRating = Integer.parseInt(oxygenRating(bitIndex = 0, diagnosticReport).first(), 2)
+    val co2ScrubberRating = Integer.parseInt(co2ScrubberRating(bitIndex = 0, diagnosticReport).first(), 2)
 
+    println("oxygenRating: $oxygenRating, co2ScrubberRating: $co2ScrubberRating, product: ${oxygenRating * co2ScrubberRating}")
+}
+
+private fun oxygenRating(bitIndex: Int, list: List<String>): List<String> {
+    if (list.size == 1) {
+        return list
+    }
+
+    val numZeros = list.count { it[bitIndex] == '0' }
+    val numOnes = list.count { it[bitIndex] == '1' }
+    val valuesWithAZero = list.filter { it[bitIndex] == '0' }
+    val valuesWithAOne = list.filter { it[bitIndex] == '1' }
+
+    return if (numZeros > numOnes) {
+        oxygenRating(bitIndex = bitIndex + 1, valuesWithAZero)
+    } else {
+        oxygenRating(bitIndex = bitIndex + 1, valuesWithAOne)
+    }
+}
+
+private fun co2ScrubberRating(bitIndex: Int, list: List<String>): List<String> {
+    if (list.size == 1) {
+        return list
+    }
+
+    val numZeros = list.count { it[bitIndex] == '0' }
+    val numOnes = list.count { it[bitIndex] == '1' }
+
+    val valuesWithAZero = list.filter { it[bitIndex] == '0' }
+    val valuesWithAOne = list.filter { it[bitIndex] == '1' }
+
+    println(
+        "0s: $numZeros, 1s: $numOnes, val0s: ${valuesWithAZero.joinToString(",")}, val1s: ${
+            valuesWithAOne.joinToString(
+                ","
+            )
+        }"
+    )
+    return if (numZeros > numOnes) {
+        co2ScrubberRating(bitIndex = bitIndex + 1, valuesWithAOne)
+    } else {
+        co2ScrubberRating(bitIndex = bitIndex + 1, valuesWithAZero)
+    }
 }
 
 internal fun part1(diagnosticReport: List<String>) {
     var gammaRate = ""
     var epsilonRate = ""
 
-    for (index in 0 until diagnosticReport[0].length) {
-        val numZeros = diagnosticReport.count { it[index] == '0' }
-        val numOnes = diagnosticReport.count { it[index] == '1' }
+    for (bitIndex in 0 until diagnosticReport[0].length) {
+        val numZeros = diagnosticReport.count { it[bitIndex] == '0' }
+        val numOnes = diagnosticReport.count { it[bitIndex] == '1' }
 
         if (numZeros > numOnes) {
             gammaRate += "0"
@@ -29,7 +74,6 @@ internal fun part1(diagnosticReport: List<String>) {
             gammaRate += "1"
             epsilonRate += "0"
         }
-        println("0s: $numZeros, 1s: $numOnes, gam: $gammaRate, ep: $epsilonRate")
     }
     val gamma = Integer.parseInt(gammaRate, 2)
     val epsilon = Integer.parseInt(epsilonRate, 2)
