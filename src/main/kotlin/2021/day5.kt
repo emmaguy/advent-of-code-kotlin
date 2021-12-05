70354aa50171730250e91ad168f20e47b589b779
Emma Vanbrabant <emmag87@gmail.com>
2021-12-05 09:42:05 +0000

    make space for part 2

--- src/main/kotlin/2021/day5.kt
+++ src/main/kotlin/2021/day5.kt
@@ -4,20 +4,14 @@ import java.io.File
 
 internal val ventLinesSample = File("src/main/resources/2021/day5-sample.txt")
     .readLines()
-    .toInput()
 internal val ventLines = File("src/main/resources/2021/day5.txt")
     .readLines()
-    .toInput()
 
 fun main() {
-    part1(ventLines)
+    calculatePoints(ventLinesSample.toInputPart2())
 }
 
-internal fun part2() {
-
-}
-
-internal fun part1(input: List<Pair<Int, Int>>) {
+internal fun calculatePoints(input: List<Pair<Int, Int>>) {
     val frequencyMap = mutableMapOf<Pair<Int, Int>, Int>()
 
     input.forEach { frequencyMap[it] = (frequencyMap[it] ?: 0) + 1 }
@@ -26,7 +20,30 @@ internal fun part1(input: List<Pair<Int, Int>>) {
     println("points: ${frequencyMap.count { it.value >= 2 }}")
 }
 
-internal fun List<String>.toInput(): List<Pair<Int, Int>> {
+// Horizontal and diagonal only
+internal fun List<String>.toInputPart1(): List<Pair<Int, Int>> {
+    return flatMap { line ->
+        val split = line.split(" ")
+        val start = split[0].split(",")
+        val end = split[2].split(",")
+
+        val x1 = start[0].toInt()
+        val y1 = start[1].toInt()
+
+        val x2 = end[0].toInt()
+        val y2 = end[1].toInt()
+
+        if (x1 == x2) {
+            (if (y2 > y1) (y1..y2) else (y2..y1)).map { Pair(x1, it) }
+        } else if (y1 == y2) {
+            (if (x2 > x1) (x1..x2) else (x2..x1)).map { Pair(it, y1) }
+        } else {
+            emptyList()
+        }
+    }
+}
+
+internal fun List<String>.toInputPart2(): List<Pair<Int, Int>> {
     return flatMap { line ->
         val split = line.split(" ")
         val start = split[0].split(",")
--- src/test/kotlin/2021/day5test.kt
+++ src/test/kotlin/2021/day5test.kt
@@ -6,27 +6,27 @@ import kotlin.test.assertEquals
 class Day5Test {
 
     @Test fun `x changes positive`() {
-        val input = listOf("1,1 -> 3,1").toInput()
+        val input = listOf("1,1 -> 3,1").toInputPart1()
         assertEquals(input, listOf(1 to 1, 2 to 1, 3 to 1))
     }
 
     @Test fun `y changes positive`() {
-        val input = listOf("1,1 -> 1,3").toInput()
+        val input = listOf("1,1 -> 1,3").toInputPart1()
         assertEquals(input, listOf(1 to 1, 1 to 2, 1 to 3))
     }
 
     @Test fun `x changes negative`() {
-        val input = listOf("9,7 -> 7,7").toInput()
+        val input = listOf("9,7 -> 7,7").toInputPart1()
         assertEquals(input, listOf(7 to 7, 8 to 7, 9 to 7))
     }
 
     @Test fun `y changes negative`() {
-        val input = listOf("7,9 -> 7,7").toInput()
+        val input = listOf("7,9 -> 7,7").toInputPart1()
         assertEquals(input, listOf(7 to 7, 7 to 8, 7 to 9))
     }
 
     @Test fun `ignore equal`() {
-        val input = listOf("8,0 -> 0,8").toInput()
+        val input = listOf("8,0 -> 0,8").toInputPart1()
         assertEquals(input, emptyList())
     }
 }
