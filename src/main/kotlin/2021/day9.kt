394b2cbb653f815d8a043d36024ec9e1012fdd10
Emma Vanbrabant <emmag87@gmail.com>
2021-12-09 19:38:33 +0000

    refactor

--- src/main/kotlin/2021/day9.kt
+++ src/main/kotlin/2021/day9.kt
@@ -19,23 +19,22 @@ fun part1(heightmap: List<String>) {
 
 internal fun List<String>.toHeightmap(): List<Point> {
     val points = mutableListOf<Point>()
-    forEachIndexed { rowIndex, s ->
-        // parse each number and get the adjacent values
-        for ((index, digit) in s.withIndex()) {
-
+    for (rowIndex in 0 until this.size) {
+        val row = this[rowIndex]
+        for ((index, digit) in row.withIndex()) {
             // Look up down left right for adjacent friends
             val adjacentValues = mutableListOf<Int>()
-            if (index - 1 >= 0) { // Left
-                adjacentValues.add(s[index - 1].digitToInt())
+            index(index, rowIndex, Direction.LEFT).takeIf { it >= 0 }?.let {
+                adjacentValues.add(row[it].digitToInt())
             }
-            if (index + 1 < s.length) { // Right
-                adjacentValues.add(s[index + 1].digitToInt())
+            index(index, rowIndex, Direction.RIGHT).takeIf { it < row.length }?.let {
+                adjacentValues.add(row[it].digitToInt())
             }
-            if (rowIndex - 1 >= 0) { // Up
-                adjacentValues.add(this[rowIndex - 1][index].digitToInt())
+            index(index, rowIndex, Direction.UP).takeIf { it >= 0 }?.let {
+                adjacentValues.add(this[it][index].digitToInt())
             }
-            if (rowIndex + 1 < size) { // Down
-                adjacentValues.add(this[rowIndex + 1][index].digitToInt())
+            index(index, rowIndex, Direction.DOWN).takeIf { it < size }?.let {
+                adjacentValues.add(this[it][index].digitToInt())
             }
 
             println("$digit has adjacent values ${adjacentValues.joinToString(",")}")
@@ -45,4 +44,14 @@ internal fun List<String>.toHeightmap(): List<Point> {
     return points
 }
 
+private fun index(columnIndex: Int, rowIndex: Int, direction: Direction): Int {
+    return when (direction) {
+        Direction.UP -> rowIndex - 1
+        Direction.DOWN -> rowIndex + 1
+        Direction.LEFT -> columnIndex - 1
+        Direction.RIGHT -> columnIndex + 1
+    }
+}
+
+enum class Direction { UP, DOWN, LEFT, RIGHT }
 data class Point(val value: Int, val adjacentPoints: List<Int>)
