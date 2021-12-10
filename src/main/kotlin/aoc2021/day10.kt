c09d5aa181cd238a0c63841c843258aa403b376b
Emma Vanbrabant <emmag87@gmail.com>
2021-12-10 19:54:31 +0000

    refactor

--- src/main/kotlin/aoc2021/day10.kt
+++ src/main/kotlin/aoc2021/day10.kt
@@ -5,151 +5,132 @@ import java.io.File
 internal val subsystemInput = File("src/main/resources/2021/day10.txt").readLines()
 internal val subsystemSample = File("src/main/resources/2021/day10-sample.txt").readLines()
 
+private val openToClose = mapOf(
+    '(' to ')',
+    '[' to ']',
+    '{' to '}',
+    '<' to '>',
+)
+
+private val illegalCharScores = mapOf(
+    ')' to 3,
+    ']' to 57,
+    '}' to 1197,
+    '>' to 25137,
+)
+
+private val charScores = mapOf(
+    ')' to 1L,
+    ']' to 2L,
+    '}' to 3L,
+    '>' to 4L,
+)
+
 fun main() {
     part1(listOf("[({(<(())[]>"))
     part1(listOf("{([(<{}[<>[]}>{[]{[(<()>"))
     part1(subsystemSample)
+    part1Alt(subsystemSample)
     part1(subsystemInput)
 
+    println("\n\n")
+
+    part2(listOf("[({(<(())[]>"))
+    part2(listOf("<{([{{}}[<[[[<>{}]]]>[]]"))
+    part2(subsystemSample)
     part2(subsystemInput)
 }
 
 private fun part2(input: List<String>) {
     val rows = input.mapNotNull { row ->
-        println(row)
-
         val root = Node(row[0])
         var currentNode = root
         for (i in 1 until row.length) {
             val char = row[i]
             val node = Node(char)
-            if (char == '(' || char == '{' || char == '[' || char == '<') {
+            if (openToClose.containsKey(char)) { // Open char e.g. '('
                 currentNode.addChild(node)
                 currentNode = node
-            } else if (char == ')' || char == '}' || char == ']' || char == '>') {
+            } else {
                 val parent = currentNode.parent ?: return@mapNotNull null
                 parent.addChild(node)
                 currentNode = parent
             }
         }
-        root.printTree()
-        println()
-        print("missing closes: ")
-        val missingCloses = root.missingCloses() ?: return@mapNotNull null
-        val total = missingCloses
-            .map {
-                print("$it")
-                when (it) {
-                    ')' -> 1L
-                    ']' -> 2L
-                    '}' -> 3L
-                    '>' -> 4L
-                    else -> throw RuntimeException("invalid char: $it")
-                }
-            }
-            .reduce { acc, i -> acc * 5L + i }
 
-        println()
-        println("total: $total")
-        total
+        // Exclude invalid rows
+        val missingCloseChars = root.missingCloseChars() ?: return@mapNotNull null
+
+        missingCloseChars.map { charScores[it]!! }
+            .reduce { acc, i -> acc * 5L + i }
     }.sorted().toList()
 
-    println("$rows")
-    println("half: ${rows[rows.size / 2]}")
+    println("part 2 middle score: ${rows[rows.size / 2]}")
 }
 
-private fun Node<Char>.isValid(): Char? {
-    closeCharOrNull(openChar = '(', closeChar = ')', children)?.let { return it }
-    closeCharOrNull(openChar = '[', closeChar = ']', children)?.let { return it }
-    closeCharOrNull(openChar = '{', closeChar = '}', children)?.let { return it }
-    closeCharOrNull(openChar = '<', closeChar = '>', children)?.let { return it }
+// Checks the node and all children below are valid, returns first non-valid char
+private fun Node<Char>.findFirstNonValidChar(): Char? {
+    openToClose.forEach { (openChar, closeChar) ->
+        val opens = children.filter { it.value == openChar }
+        val closes = children.filter { it.value == closeChar }
+        if (closes.size > opens.size) {
+            return closeChar
+        }
+    }
 
     for (c in children) {
         // any close brackets without an open?
-        c.isValid()?.let { return it }
+        c.findFirstNonValidChar()?.let { return it }
     }
     return null
 }
 
 private fun part1(input: List<String>) {
-    val groups = input.mapNotNull { row ->
-        println(row)
-
+    val score = input.mapNotNull { row ->
         val root = Node(row[0])
         var currentNode = root
         for (i in 1 until row.length) {
             val char = row[i]
             val node = Node(char)
-            if (char == '(' || char == '{' || char == '[' || char == '<') {
+            if (openToClose.containsKey(char)) { // Open char e.g. '('
                 currentNode.addChild(node)
                 currentNode = node
-            } else if (char == ')' || char == '}' || char == ']' || char == '>') {
+            } else {
                 val parent = currentNode.parent ?: break
                 parent.addChild(node)
                 currentNode = parent
             }
         }
-        root.printTree()
+        root.findFirstNonValidChar()
+    }.sumOf { illegalCharScores[it]!! }
 
-        val firstInvalidChar = root.isValid()
-        println("\n\nfirstInvalidChar: $firstInvalidChar")
-        firstInvalidChar
-    }
-        .map {
-            if (it == ')') 3
-            else if (it == ']') 57
-            else if (it == '}') 1197
-            else if (it == '>') 25137
-            else throw RuntimeException("invalid char: $it")
-        }.sum()
-    println("$groups")
+    println("part1 score: $score")
 }
 
-private fun Node<Char>.missingCloses(): List<Char>? {
+private fun Node<Char>.missingCloseChars(): List<Char>? {
     if (children.isEmpty()) return emptyList()
+
     val chars = mutableListOf<Char>()
     children.forEach { c ->
-        val list = c.missingCloses() ?: return null
+        val list = c.missingCloseChars() ?: return null
         chars.addAll(list)
     }
-
-    // exclude corrupt lines
-    closeCharOrNull(openChar = '(', closeChar = ')', children)?.let { return null }
-    closeCharOrNull(openChar = '[', closeChar = ']', children)?.let { return null }
-    closeCharOrNull(openChar = '{', closeChar = '}', children)?.let { return null }
-    closeCharOrNull(openChar = '<', closeChar = '>', children)?.let { return null }
-
-    openCharOrNull(openChar = '[', closeChar = ']', children)?.let { chars.add(it) }
-    openCharOrNull(openChar = '(', closeChar = ')', children)?.let { chars.add(it) }
-    openCharOrNull(openChar = '{', closeChar = '}', children)?.let { chars.add(it) }
-    openCharOrNull(openChar = '<', closeChar = '>', children)?.let { chars.add(it) }
+    openToClose.forEach { (openChar, closeChar) ->
+        val opens = children.filter { it.value == openChar }
+        val closes = children.filter { it.value == closeChar }
+        if (closes.size > opens.size) {
+            // Invalid row
+            return null
+        }
+        if (opens.size > closes.size) {
+            chars.add(closeChar)
+        }
+    }
 
     if (parent == null) {
-        if (value == '(') chars.add(')')
-        if (value == '[') chars.add(']')
-        if (value == '{') chars.add('}')
-        if (value == '<') chars.add('>')
+        chars.add(openToClose[value]!!)
     }
-
     return chars
-}
-
-private fun openCharOrNull(openChar: Char, closeChar: Char, children: MutableList<Node<Char>>): Char? {
-    val opens = children.filter { it.value == openChar }
-    val closes = children.filter { it.value == closeChar }
-    if (opens.size > closes.size) {
-        return closeChar
-    }
-    return null
-}
-
-private fun closeCharOrNull(openChar: Char, closeChar: Char, children: MutableList<Node<Char>>): Char? {
-    val opens = children.filter { it.value == openChar }
-    val closes = children.filter { it.value == closeChar }
-    if (closes.size > opens.size) {
-        return closeChar
-    }
-    return null
 }
 
 private fun <T> Node<T>.printTree() {
@@ -174,12 +155,6 @@ class Node<T>(var value: T) {
 }
 
 /// ALT
-private val openToClose = mapOf(
-    '(' to ')',
-    '[' to ']',
-    '{' to '}',
-    '<' to '>',
-)
 
 private fun part1Alt(input: List<String>) {
     val firstIncorrectClosingChar = mutableListOf<Char>()
@@ -193,7 +168,6 @@ private fun part1Alt(input: List<String>) {
                 stack.push(it)
             } else {
                 if (it != openToClose[stack.peek()]) {
-                    println("invalid row $it, peek: ${stack.peek()}")
                     firstIncorrectClosingChar.add(it)
                     break
                 } else {
@@ -203,17 +177,8 @@ private fun part1Alt(input: List<String>) {
         }
     }
 
-    val errorScore = firstIncorrectClosingChar
-        .map {
-            when (it) {
-                ')' -> 3
-                ']' -> 57
-                '}' -> 1197
-                '>' -> 25137
-                else -> throw RuntimeException("invalid char: $it")
-            }
-        }.sum()
-    println("$errorScore")
+    val errorScore = firstIncorrectClosingChar.sumOf { illegalCharScores[it]!! }
+    println("part1 alt score: $errorScore")
 }
 
 fun <T> MutableList<T>.push(item: T) = this.add(this.count(), item)
